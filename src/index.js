63eb0941e88b309a8f602b43ae28de8973d098b6
stephane18-smith <poutcheustephane18@gmail.com>
2026-05-21 10:47:38 +0100

    Refactor index.js to remove HashRouter
    Removed HashRouter wrapping from the App component.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>
 );
