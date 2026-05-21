5f21011381dc2af61b912df7d4e630bbf2b438ec
stephane18-smith <poutcheustephane18@gmail.com>
2026-05-21 10:52:23 +0100

    Add console logs to App component
    Added console logs for debugging during component load and render.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import PortfolioHome from './pages/PortfolioHome';
 import './App.css';
 
+console.log('App.js chargé');
+
 function App() {
+  console.log('App component render');
   return (
     <Router>
       <div className="app">
